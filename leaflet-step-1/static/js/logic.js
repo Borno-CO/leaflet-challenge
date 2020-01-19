6eaa0d8bd6c138a132e4a78ff15e304ea7d7532b
Borno-CO <53917197+Borno-CO@users.noreply.github.com>
2020-01-19 11:22:04 -0700

    circles fixed, plates added

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,5 +1,5 @@
 // set the url to a variable
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
 
 
 // Perform a GET request to the query URL
@@ -10,23 +10,23 @@ d3.json(url, function(data) {
 
 // Define a markerSize function that set the radius of the circle markers based quake significance rating
 function markerSize(significance) {
-  return significance * 10;
+  return significance / 20;
 };
 
 function markerColor(magnitude) {
-  if (magnitude < 2.5) {
+  if (magnitude < 2.0) {
     color = "#ffffcc";
   }
+  // else if (magnitude < 3.0) {
+  //   color = "#ffeda0";
+  // }
   else if (magnitude < 3.0) {
-    color = "#ffeda0";
-  }
-  else if (magnitude < 3.5) {
     color = "#fed976";
   }
+  // else if (magnitude < 4.0) {
+  //   color = "#feb24c";
+  // }
   else if (magnitude < 4.0) {
-    color = "#feb24c";
-  }
-  else if (magnitude < 4.5) {
     color = "#fd8d3c";
   }
   else if (magnitude < 5.0) {
@@ -41,7 +41,8 @@ function markerColor(magnitude) {
   else {
     color = "#800026";
   }
-}
+  return color
+};
 
   // create variable for the color of the circle markers
   // var color = "";
@@ -56,48 +57,35 @@ function createFeatures(earthquakeData) {
 
     var markerRadius = markerSize(feature.properties.sig);
     console.log(`radius: ${markerRadius}`);
-
-    var circleColor = markerColor(feature.properties.mag)
+    var magValue = feature.properties.mag;
+    console.log(`magValue: ${magValue}`);
+    var circleColor = markerColor(feature.properties.mag);
     console.log(`color: ${circleColor}`);
 
     var geojsonMarkerOptions = {
       radius: markerRadius,
       fillColor: circleColor,
-      // color: "#000",
-      // weight: 1,
+      color: circleColor,
+      weight: 1,
       // opacity: 1,
-      fillOpacity: 0.8
+      fillOpacity: 0.5
     };
     
-    var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]
+    // var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]
     console.log(`latlng: ${latlng}`);
-
-    L.circle(latlng, geojsonMarkerOptions)
-      .bindPopup("<lu><strong>" + feature.properties.place + "</strong>" +
-      "<li>Magnitude: " + feature.properties.mag + 
-      "</li><li>Significance: " + feature.properties.sig + 
-      "</li><li>Felt Reports: " + feature.properties.felt +
-      "</li><a href=" + feature.properties.url + ">Event USGS URL</a></lu>");
+    
+      return L.circleMarker(latlng, geojsonMarkerOptions)
+        .bindPopup("<lu><strong>" + feature.properties.place + "</strong>" +
+        "<li>Magnitude: " + feature.properties.mag + 
+        "</li><li>Significance: " + feature.properties.sig + 
+        "</li><li>Felt Reports: " + feature.properties.felt +
+        "</li><a href=" + feature.properties.url + ">Event USGS URL</a></lu>");
     
   };
 
-  // function onEachFeature(feature, layer) {
-  // //   L.circle(location, {
-  // //     fillOpacity: 0.5,
-  // //     fillcolor: 'red',
-  // //     radius: markerSize(feature.properties.sig)
-  // // })
-  //   layer.bindPopup("<lu><strong>" + feature.properties.place + "</strong>" +
-  //       "<li>Magnitude: " + feature.properties.mag + 
-  //       "</li><li>Significance: " + feature.properties.sig + 
-  //       "</li><li>Felt Reports: " + feature.properties.felt +
-  //       "</li><a href=" + feature.properties.url + ">Event USGS URL</a></lu>");
-  // };
-    
   
-  var earthquakes = L.geoJSON(earthquakeData, {
+  var earthquakes = L.geoJSON(earthquakeData,    {
     pointToLayer: pointToLayer
-    // onEachFeature: onEachFeature
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -131,7 +119,7 @@ function createMap(earthquakes) {
   
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
-      Earthquakes: earthquakes,
+      Earthquakes: earthquakes
     };
   
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -149,4 +137,12 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
-  }
+  
+
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+      function (platedata) {
+        var plates = L.geoJSON(platedata, {
+          color: "orange", weight: 2}).addTo(myMap);
+      });
+
+}
